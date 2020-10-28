72a7a7f48294ac2e9ae0bbe3718814613aed17b7
j118172 <j118172@ns.kogakuin.ac.jp>
2020-10-28 16:06:36 +0900

    add html and change branch

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'staticpages/home'
-  get 'staticpages/help'
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'static_pages/about'
     root 'application#hello'
 end
